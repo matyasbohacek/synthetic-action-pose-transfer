9b422edf9c7225671c21f0e30cb98d77ec6ef917
matyasbohacek <matyas.bohacek@matsworld.io>
2025-01-05 07:57:14 -0800

    add shell scripts for few-shot experiments

---
+++ experiments/shell-scripts/run-few-shot-real-and-synth.sh
@@ -0,0 +1,69 @@
+python train.py \
+    --baseline_model "facebook/timesformer-base-finetuned-k400" \
+    --training_sources "real" "background" \
+    --class_cap_per_dir__real 1 \
+    --class_cap_per_dir__background 225 \
+    --experiment_name "granular-progress-1"
+
+python train.py \
+    --baseline_model "facebook/timesformer-base-finetuned-k400" \
+    --training_sources "real" "background" \
+    --class_cap_per_dir__real 2 \
+    --class_cap_per_dir__background 225 \
+    --experiment_name "granular-progress-2"
+
+python train.py \
+    --baseline_model "facebook/timesformer-base-finetuned-k400" \
+    --training_sources "real" "background" \
+    --class_cap_per_dir__real 3 \
+    --class_cap_per_dir__background 225 \
+    --experiment_name "granular-progress-3"
+
+python train.py \
+    --baseline_model "facebook/timesformer-base-finetuned-k400" \
+    --training_sources "real" "background" \
+    --class_cap_per_dir__real 4 \
+    --class_cap_per_dir__background 225 \
+    --experiment_name "granular-progress-4"
+
+python train.py \
+    --baseline_model "facebook/timesformer-base-finetuned-k400" \
+    --training_sources "real" "background" \
+    --class_cap_per_dir__real 5 \
+    --class_cap_per_dir__background 225 \
+    --experiment_name "granular-progress-5"
+
+python train.py \
+    --baseline_model "facebook/timesformer-base-finetuned-k400" \
+    --training_sources "real" "background" \
+    --class_cap_per_dir__real 6 \
+    --class_cap_per_dir__background 225 \
+    --experiment_name "granular-progress-6"
+
+python train.py \
+    --baseline_model "facebook/timesformer-base-finetuned-k400" \
+    --training_sources "real" "background" \
+    --class_cap_per_dir__real 7 \
+    --class_cap_per_dir__background 225 \
+    --experiment_name "granular-progress-7"
+
+python train.py \
+    --baseline_model "facebook/timesformer-base-finetuned-k400" \
+    --training_sources "real" "background" \
+    --class_cap_per_dir__real 8 \
+    --class_cap_per_dir__background 225 \
+    --experiment_name "granular-progress-8"
+
+python train.py \
+    --baseline_model "facebook/timesformer-base-finetuned-k400" \
+    --training_sources "real" "background" \
+    --class_cap_per_dir__real 9 \
+    --class_cap_per_dir__background 225 \
+    --experiment_name "granular-progress-9"
+
+python train.py \
+    --baseline_model "facebook/timesformer-base-finetuned-k400" \
+    --training_sources "real" "background" \
+    --class_cap_per_dir__real 10 \
+    --class_cap_per_dir__background 225 \
+    --experiment_name "granular-progress-10"
---
+++ experiments/shell-scripts/run-few-shot-real.sh
@@ -0,0 +1,59 @@
+python train.py \
+    --baseline_model "facebook/timesformer-base-finetuned-k400" \
+    --training_sources "real" \
+    --class_cap_per_dir__real 1 \
+    --experiment_name "granular-progress-1---sm"
+
+python train.py \
+    --baseline_model "facebook/timesformer-base-finetuned-k400" \
+    --training_sources "real" \
+    --class_cap_per_dir__real 2 \
+    --experiment_name "granular-progress-2---sm"
+
+python train.py \
+    --baseline_model "facebook/timesformer-base-finetuned-k400" \
+    --training_sources "real" \
+    --class_cap_per_dir__real 3 \
+    --experiment_name "granular-progress-3---sm"
+
+python train.py \
+    --baseline_model "facebook/timesformer-base-finetuned-k400" \
+    --training_sources "real" \
+    --class_cap_per_dir__real 4 \
+    --experiment_name "granular-progress-4---sm"
+
+python train.py \
+    --baseline_model "facebook/timesformer-base-finetuned-k400" \
+    --training_sources "real" \
+    --class_cap_per_dir__real 5 \
+    --experiment_name "granular-progress-5---sm"
+
+python train.py \
+    --baseline_model "facebook/timesformer-base-finetuned-k400" \
+    --training_sources "real"  \
+    --class_cap_per_dir__real 6 \
+    --experiment_name "granular-progress-6---sm"
+
+python train.py \
+    --baseline_model "facebook/timesformer-base-finetuned-k400" \
+    --training_sources "real"  \
+    --class_cap_per_dir__real 7 \
+    --experiment_name "granular-progress-7---sm"
+
+python train.py \
+    --baseline_model "facebook/timesformer-base-finetuned-k400" \
+    --training_sources "real"  \
+    --class_cap_per_dir__real 8 \
+    --experiment_name "granular-progress-8---sm"
+
+python train.py \
+    --baseline_model "facebook/timesformer-base-finetuned-k400" \
+    --training_sources "real"  \
+    --class_cap_per_dir__real 9 \
+    --experiment_name "granular-progress-9---sm"
+
+python train.py \
+    --baseline_model "facebook/timesformer-base-finetuned-k400" \
+    --training_sources "real" \
+    --class_cap_per_dir__real 10 \
+    --experiment_name "granular-progress-10---sm"
