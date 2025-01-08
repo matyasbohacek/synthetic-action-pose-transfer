b8446786527334d4c9caa450124a1a974c6f3d50
Vaclav Knapp <131234769+VaclavKnapp@users.noreply.github.com>
2025-01-08 23:03:13 +0100

    Update n_frames.py

--- synthetic_data_generation/n_frames.py
+++ synthetic_data_generation/n_frames.py
@@ -10,40 +10,40 @@ def parse_args():
     assert args.root_path, "Please set root_path."
     return args
 
-# get path
+
 args = parse_args()
 root_path = args.root_path
 
-# Path to the video file
+
 video_path = os.path.join(root_path, 'video.mp4')
 print(video_path)
 
-# Initialize the video capture
+
 cap = cv2.VideoCapture(video_path)
 
 
-# Get the total number of frames
+
 total_frames = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
 
-# Close the video capture
+
 cap.release()
 
 all_txt = os.path.join(root_path, 'frame_list_all.txt')
 train_txt = os.path.join(root_path, 'frame_list_train.txt')
 test_txt = os.path.join(root_path, 'frame_list_test.txt') 
 
-# Create and write to frame_list_all.txt
+
 with open(all_txt, 'w') as f_all:
     for i in range(total_frames):
         f_all.write(f"{i}\n")
 
-# Create and write to frame_list_train.txt (every 5th frame)
+
 with open(train_txt, 'w') as f_train:
-    for i in range(4, total_frames, 6):  # Start from frame 4, then take every 6th frame (4, 10, 16,...)
+    for i in range(4, total_frames, 6):  
         f_train.write(f"{i}\n")
 
 with open(test_txt, 'w') as f_train:
-    for i in range(2, total_frames, 3):  # Start from frame 4, then take every 6th frame (4, 10, 16,...)
+    for i in range(2, total_frames, 3): 
         f_train.write(f"{i}\n")
 
 
